80f443c9e0cf857f02087b0450fcf06c9bfb38b3
Chen Shen <cc-shen@users.noreply.github.com>
2015-02-01 00:27:47 -0500

    Update app.js
    Added comments and global variables for easier modifications

--- app.js
+++ app.js
@@ -1,73 +1,70 @@
+// app.js
 
-// Your accountSid and authToken from twilio.com/user/account
-var accountSid = 'ACfe2ccf1f441035f831fb78424bf4568e';
+// Your accountSid, authToken, and twilio number from twilio.com/user/account
+var accountSid = '[Account Sid]';
 var authToken = '[Authorization Code]';
+var twilioNumber = '+1[twilio-number]';
+
+// Obtaining all required node.js modules
 var client = require('twilio')(accountSid, authToken);
 var unirest = require('unirest');
 var express = require("express");
 var http = require('http');
-var app = express();
 var o2x = require('object-to-xml');
 var qs = require('querystring');
 
+var app = express();
+
+// Defaults (Fallbacks)
 var twiml2 = "https://api.twilio.com/cowbell.mp3";
 var rickRoll = "http://demo.twilio.com/docs/voice.xml";
 var search = "aaaambient";
-
-
 
 app.get("/test", function (req, res) {
 	res.set("Content-Type", "text/xml");
 	res.send(o2x({
 		'?xml version="1.0" encoding="utf-8"?' : null,
 		Response: {
-				Play: twiml2
+			Play: twiml2
 		}
 	}));
 
-  console.log(req.query.From);
 	req.setEncoding('utf8');
+
+	// Get user's input (song name + artist)
 	var input = "" + req.query.From;
 	search = "" + req.query.Body;
-
 	var newSearch = "";
+
+	// Eliminate spaces
 	for (var i = 0; i<search.length; i++){
-		if (search.charAt(i) == ' '){
+		if (search.charAt(i) == ' ') {
 			newSearch += '+';
 		}
 		else {
 			newSearch += search.charAt(i);
 		}
 	}
-	console.log(req.query.Body);
+
+	// Requests Deezer API
 	unirest.get("https://deezerdevs-deezer.p.mashape.com/search?q=" + newSearch)
 	.header("X-Mashape-Key", "mpRdHHU7R9mshNH0entzFSW4Akttp1ScydejsnKby6lb2BoCeY")
 	.header("Accept", "text/plain")
 	.end(function (result) {
-		//console.log(result.body);
-		console.log("1");
-		//console.log(result.body.data);
-		console.log("2");
-		//console.log(result.body.total);
-		//console.log(twiml2);
-		//console.log(newSearch);
-		console.log("3");
-		// Checks if the song is found or not
+		// Song is not found
 		if (result === null || result.body.total <= 0) {
 			twiml2 = rickroll;
 		}
+		// Song is found
 		else {
-			console.log(twiml2);
 			twiml2 = result.body.data[0].preview;
 		}
-		console.log(twiml2);
-		console.log("4");
+
+		// Make the call to the client 
 		if (input != null && typeof input == "string"){
-			//console.log(input);
-			//console.log(twiml2 + " something");
 			client.calls.create({
 				to: input,
-				from: "+1 [Twilio-number]",
+				from: twilioNumber,
 				url: twiml2,
 				method: "GET",
 				fallbackMethod: "GET",
@@ -76,37 +73,23 @@ app.get("/test", function (req, res) {
 			}, function(err, call) {
 				console.log(call.sid);
 			});
-
-		} else {
-				console.log(input);
-				client.messages.create({
-							body: input + " error",
-							to: "+1 [personal-number]",
-							from: "+1 [Twilio-number]"
-					}, function(err, message) {
-						console.log(err);
-							//process.stdout.write(message.sid);
-					});
+		} 
+		// Send an error message
+		else {
+			client.messages.create({
+				body: input + " error",
+				to: "+1 [personal-number]",
+				from: twilioNumber
+			}, function(err, message) {
+				console.log(err);
+			});
 		}
 	});
-
-
-// client.messages.create({
-// 			body: input + " no error",
-// 			to: input,
-// 			from: "+1 [Twilio-number]"
-// 	}, function(err, message) {
-// 		console.log(err);
-// 		//process.stdout.write(err);
-// 	});
-
 });
 
+// Starts the server and listen for inputs
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 // The port on the DEA for communication with the application:
 var port = (process.env.VCAP_APP_PORT || 8080);
 //Start server
 app.listen(port, host);
-// http.createServer(app).listen(port,function(){
-// 	console.log("Listening on port " + port);
-// });
